4f820ecfe993d3a1864f0b7f944b2c2ee9c207bd
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-01-15 20:43:43 +0900

    :sparkles: Benchmark gen32::operator() vs dist32(gen64)

--- c++/benchmark.cpp
+++ c++/benchmark.cpp
@@ -7,29 +7,34 @@
 
 #include <functional>
 
-int main() {
-    constexpr size_t n = 2 * 1000 * 1000;
-    std::vector<size_t> x(n);
-
-    double mu = 8.0;
-    std::poisson_distribution<size_t> dist(mu);
-
-    auto lambda_random = [&](auto& generator) mutable {
-        for (size_t j=0; j<n; ++j) {
-            x[j] = dist(generator);
+inline void random_uint32() {
+    constexpr size_t n = 4 * 1000 * 1000;
+    uint32_t trash = 0;
+    wtl::benchmark([&](){
+        for (size_t i=0; i<n; ++i) {
+            trash += wtl::sfmt()();
         }
-    };
-    wtl::benchmark(std::bind(lambda_random, wtl::mt()), "mt", 2);
-    std::cerr << wtl::mean(x) << std::endl;
-    wtl::benchmark(std::bind(lambda_random, wtl::mt64()), "mt64", 2);
-    std::cerr << wtl::mean(x) << std::endl;
-    wtl::benchmark(std::bind(lambda_random, wtl::sfmt()), "sfmt", 2);
-    std::cerr << wtl::mean(x) << std::endl;
-    wtl::benchmark(std::bind(lambda_random, wtl::sfmt64()), "sfmt64", 2);
-    std::cerr << wtl::mean(x) << std::endl;
+    }, "gen32::operator()");
+    std::cerr << trash << std::endl;
+    wtl::benchmark([&](){
+        std::uniform_int_distribution<uint32_t> unif;
+        for (size_t i=0; i<n; ++i) {
+            trash += unif(wtl::sfmt64());
+        }
+    }, "unif32(gen64)");
+    std::cerr << trash << std::endl;
+}
 
-    size_t k = n / 50;
+inline void random_sample() {
+    constexpr size_t n = 4 * 1000 * 1000;
+    std::vector<size_t> x(n);
+    std::iota(x.begin(), x.end(), 0u);
+    size_t k = n / 40u;
     size_t trash = 0;
+    wtl::benchmark([&](){
+        trash += *wtl::sample(n, k, wtl::sfmt64()).begin();
+    }, "sample int");
+    std::cerr << trash << std::endl;
     wtl::benchmark([&](){
         trash += wtl::sample(x, k, wtl::sfmt64())[0];
     }, "sample");
@@ -48,4 +53,32 @@ int main() {
     std::cerr << trash << std::endl;
     x.resize(6);
     std::cerr << x << std::endl;
+}
+
+inline void random_engine() {
+    constexpr size_t n = 1 * 1000 * 1000;
+    std::vector<size_t> v(n);
+    std::poisson_distribution<size_t> dist(2.0);
+    // std::vector<double> v(n);
+    // std::normal_distribution<double> dist(0.0, 1.0);
+    // std::uniform_real_distribution<double> dist(0.0, 1.0);
+    auto lambda_random = [&](auto& generator) mutable {
+        for (size_t j=0; j<n; ++j) {
+            v[j] = dist(generator);
+        }
+    };
+    wtl::benchmark(std::bind(lambda_random, wtl::mt()), "mt", 2);
+    std::cerr << wtl::mean(v) << std::endl;
+    wtl::benchmark(std::bind(lambda_random, wtl::mt64()), "mt64", 2);
+    std::cerr << wtl::mean(v) << std::endl;
+    wtl::benchmark(std::bind(lambda_random, wtl::sfmt()), "sfmt", 2);
+    std::cerr << wtl::mean(v) << std::endl;
+    wtl::benchmark(std::bind(lambda_random, wtl::sfmt64()), "sfmt64", 2);
+    std::cerr << wtl::mean(v) << std::endl;
+}
+
+int main() {
+    random_uint32();
+    random_sample();
+    random_engine();
 }
