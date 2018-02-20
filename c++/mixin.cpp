6b8236f477894d9685b029f3d38416c9884a2ea2
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-02-21 02:25:03 +0900

    :sparkles: Import mixin.cpp

---
+++ c++/mixin.cpp
@@ -0,0 +1,54 @@
+#include <mutex>
+#include <memory>
+#include <iostream>
+
+template <class T>
+class Singleton {
+  public:
+    template <typename... Args>
+    static T& instance(Args... args) {
+        std::call_once(once_flag_, init<Args...>, std::forward<Args>(args)...);
+        return *instance_;
+    }
+
+    static void destroy() {
+        instance_.reset(nullptr);
+    }
+
+  protected:
+    Singleton() = default;
+    ~Singleton() = default;
+
+  private:
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+
+    template <typename... Args>
+    static void init(Args... args) {
+        instance_.reset(new T(std::forward<Args>(args)...));
+    }
+
+    static std::once_flag once_flag_;
+    static std::unique_ptr<T> instance_;
+};
+template <class T> std::once_flag Singleton<T>::once_flag_;
+template <class T> std::unique_ptr<T> Singleton<T>::instance_ = nullptr;
+
+// usage:
+//  - public inheritance
+//  - private constructor
+//  - friend class Singleton<T>;
+
+class Cls : public Singleton<Cls> {
+  public:
+    const int whoami;
+  private:
+    Cls(int x): whoami(x) {}
+    friend Singleton<Cls>;
+};
+
+int main() {
+    std::cout << Cls::instance(24601).whoami << "\n";
+    std::cout << Cls::instance(42).whoami << "\n";
+    return 0;
+}
