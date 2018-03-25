351d6f73f3fa0935d6a15896cad1bdbe639eb76e
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-03-26 00:35:13 +0900

    :sparkles: Create ansi_escape.py

---
+++ python/ansi_escape.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+ANSI escape sequence
+https://pypi.python.org/pypi/colorama/
+"""
+
+
+def print_bold(text, **kwargs):
+    print('\033[1m' + text + '\033[0m', **kwargs)
+
+
+def main():
+    print_bold('Hello, world!')
+
+
+if __name__ == '__main__':
+    main()
