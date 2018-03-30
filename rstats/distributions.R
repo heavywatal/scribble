8e52ee215cfb3c8127bc5862e3d6d588d39b4b9d
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-03-30 23:52:39 +0900

    :sparkles: Create distributions.R

---
+++ rstats/distributions.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+mu = 20
+n = 10000L
+k_range = c(0.4, 1, 10, 100)
+.df = k_range %>% purrr::map_dfr(~{
+    k = .x
+    prob = k / (k + mu)
+    .tex = tibble(x = rexp(n, 1 / mu), dist = "exp") %>% dplyr::filter(k == 1)
+    .tge = tibble(x = rgeom(n, 1 / mu), dist = "geom") %>% dplyr::filter(k == 1)
+    .tga = tibble(x = rgamma(n, k, scale=(1 - prob) / prob), dist = "gamma")
+    .tnb = tibble(x = rnbinom(n, k, prob), dist = "nbinom")
+    .tpo = tibble(x = rpois(n, mu), dist = "pois") %>% dplyr::filter(k == max(.grid$k))
+    .tno = tibble(x = rnorm(n, mu, sqrt(mu)), dist = "norm") %>% dplyr::filter(k == max(.grid$k))
+    dplyr::bind_rows(.tex, .tge, .tga, .tnb, .tpo, .tno) %>%
+    dplyr::mutate(k = k)
+  }) %>% print()
+
+ggplot(.df, aes(x))+
+  geom_histogram(bins=81, center=0)+
+  xlim(-0.5, 80.5)+
+  facet_grid(k ~ dist, labeller=label_both)+
+  theme_bw()
