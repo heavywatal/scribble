cd31f0cd3d6b28dbeb178d3427f964bca7bea730
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-04-11 23:35:06 +0900

    :art: Use static_assert

--- cxx/bitwise.cpp
+++ cxx/bitwise.cpp
@@ -1,46 +1,44 @@
-#include <wtl/exception.hpp>
-
 #include <cstdint>
 #include <iostream>
 
 inline void uint_int() {
-    WTL_ASSERT(uint8_t(0b00000000) ==    0u);
-    WTL_ASSERT(uint8_t(0b10000000) ==  128u);
-    WTL_ASSERT(uint8_t(0b11111111) ==  255u);
-    WTL_ASSERT( int8_t(0b01111111) ==  127);
-    WTL_ASSERT( int8_t(0b10000000) == -128);
-    WTL_ASSERT( int8_t(0b10000001) == -127);
-    WTL_ASSERT( int8_t(0b10000010) == -126);
-    WTL_ASSERT( int8_t(0b11111101) ==   -3);
-    WTL_ASSERT( int8_t(0b11111110) ==   -2);
-    WTL_ASSERT( int8_t(0b11111111) ==   -1);
+    static_assert(uint8_t(0b00000000) ==    0u, "");
+    static_assert(uint8_t(0b10000000) ==  128u, "");
+    static_assert(uint8_t(0b11111111) ==  255u, "");
+    static_assert( int8_t(0b01111111) ==  127, "");
+    static_assert( int8_t(0b10000000) == -128, "");
+    static_assert( int8_t(0b10000001) == -127, "");
+    static_assert( int8_t(0b10000010) == -126, "");
+    static_assert( int8_t(0b11111101) ==   -3, "");
+    static_assert( int8_t(0b11111110) ==   -2, "");
+    static_assert( int8_t(0b11111111) ==   -1, "");
 }
 
 inline void operation() {
-    uint8_t u = 5u;
-    WTL_ASSERT( u          == 0b00000101);
-    WTL_ASSERT((u & 1)     == 0b00000001);  //   1u
-    WTL_ASSERT((u | 2)     == 0b00000111);  //   7u
-    WTL_ASSERT((u ^ 3)     == 0b00000110);  //   6u
-    WTL_ASSERT((u << 1)    == 0b00001010);  //  10u
-    WTL_ASSERT((u >> 1)    == 0b00000010);  //   2u
-    WTL_ASSERT(uint8_t(~u) == 0b11111010u); // 250u
-    WTL_ASSERT(uint8_t(-u) == 0b11111011u); // 251u
-    WTL_ASSERT(       (~u) == 4294967290u); // NOTE: implicit ~u is not 8-bit
-    WTL_ASSERT(       (-u) == 4294967291u); // NOTE: implicit -u is not 8-bit
-    int8_t i = 5;
-    WTL_ASSERT((~i) == int8_t(0b11111010)); //  -6
-    WTL_ASSERT((-i) == int8_t(0b11111011)); //  -5
-    WTL_ASSERT((~i) ==                -6);  // NOTE: implicit ~i is not 8-bit
-    WTL_ASSERT((-i) ==                -5);  // NOTE: implicit -i is not 8-bit
-    WTL_ASSERT(250  ==        0b11111010);  // NOTE: implicit int is not 8-bit
+    constexpr uint8_t u = 5u;
+    static_assert( u          == 0b00000101, "");
+    static_assert((u & 1)     == 0b00000001, "");  //   1u
+    static_assert((u | 2)     == 0b00000111, "");  //   7u
+    static_assert((u ^ 3)     == 0b00000110, "");  //   6u
+    static_assert((u << 1)    == 0b00001010, "");  //  10u
+    static_assert((u >> 1)    == 0b00000010, "");  //   2u
+    static_assert(uint8_t(~u) == 0b11111010u, ""); // 250u
+    static_assert(uint8_t(-u) == 0b11111011u, ""); // 251u
+    static_assert(       (~u) == 4294967290u, ""); // NOTE: implicit ~u is not 8-bit
+    static_assert(       (-u) == 4294967291u, ""); // NOTE: implicit -u is not 8-bit
+    constexpr int8_t i = 5;
+    static_assert((~i) == int8_t(0b11111010), ""); //  -6
+    static_assert((-i) == int8_t(0b11111011), ""); //  -5
+    static_assert((~i) ==                -6, "");  // NOTE: implicit ~i is not 8-bit
+    static_assert((-i) ==                -5, "");  // NOTE: implicit -i is not 8-bit
+    static_assert(250  ==        0b11111010, "");  // NOTE: implicit int is not 8-bit
 }
 
 inline void right_shift() {
-    uint8_t u = 0b10000000;
-    int8_t  i = 0b10000000;
-    WTL_ASSERT((u >> 2) == uint8_t(0b00100000));
-    WTL_ASSERT((i >> 2) ==  int8_t(0b11100000)); // undefined?
+    constexpr uint8_t u = 0b10000000;
+    constexpr int8_t  i = 0b10000000;
+    static_assert((u >> 2) == uint8_t(0b00100000), "");
+    static_assert((i >> 2) ==  int8_t(0b11100000), ""); // undefined?
 }
 
 int main() {
