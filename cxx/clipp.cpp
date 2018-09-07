8a160398ab762c52880036a115c6209c7e43abb5
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-09-07 15:13:04 +0900

    :construction: Handle boolean options

--- cxx/clipp.cpp
+++ cxx/clipp.cpp
@@ -40,11 +40,6 @@ std::string doc_default(const T& x, const std::string& doc) {
   return oss.str();
 }
 
-template <class T> inline clipp::group
-clipp_group(clipp::parameter&& option, T& x, const std::string& doc="", const std::string& label="arg") {
-    return (option & clipp::value(filter_type(x), label, x)).doc(doc_default(x, doc));
-}
-
 template <typename T> inline
 auto set(nlohmann::json& obj, const char* key) {
     return [&j = obj[key]](const char* s){j = s;};
@@ -65,23 +60,53 @@ auto set<unsigned long>(nlohmann::json& obj, const char* key) {
     return [&j = obj[key]](const char* s){j = std::stoul(s);};
 }
 
+template <> inline
+auto set<bool>(nlohmann::json& obj, const char* key) {
+    return [&j = obj[key]](){j = true;};
+}
+
 template <class T> inline clipp::group
-clipp_group(nlohmann::json& obj, std::vector<std::string>&& flags, const T init=T{}, const std::string& doc="", const std::string& label="arg") {
+option(std::vector<std::string>&& flags, T* target, const std::string& doc="", const std::string& label="arg") {
+    return (
+      clipp::option(std::move(flags)) &
+      clipp::value(filter_type(*target), label, *target)
+    ) % doc_default(*target, doc);
+}
+
+template <class T> inline clipp::group
+option(nlohmann::json& obj, std::vector<std::string>&& flags, const T init, const std::string& doc="", const std::string& label="arg") {
     const char* key = flags.back().c_str();
     obj[key] = init;
-    return (clipp::option(flags)
-            & clipp::value(filter_type(init), label).call(set<T>(obj, key))
-           ).doc(doc_default(obj[key], doc));
+    return (
+      clipp::option(std::move(flags)) &
+      clipp::value(filter_type(init), label).call(set<T>(obj, key))
+    ) % doc_default(obj[key], doc);
 }
+
+template <class T> inline clipp::group
+option(nlohmann::json& obj, std::vector<std::string>&& flags, T* target, const std::string& doc="", const std::string& label="arg") {
+    auto group = option(obj, std::move(flags), *target, doc, label);
+    group.back().as_param().call(clipp::set(*target));
+    return group;
+}
+
+inline clipp::parameter
+option_bool(nlohmann::json& obj, std::vector<std::string>&& flags, const std::string& doc="") {
+    const char* key = flags.back().c_str();
+    obj[key] = false;
+    return (clipp::option(flags).call(set<bool>(obj, key))).doc(doc);
+}
+
+/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 
 class Individual {
   public:
     static clipp::group parameter_group(nlohmann::json& vm) {
         return clipp::with_prefixes_short_long("-", "--",
-          clipp_group(vm, {"d", "death"}, DEATH_RATE_, "Death rate"),
-          clipp_group(vm, {"l", "genome"}, GENOME_SIZE_, "Genome size"),
-          clipp_group(vm, {"m", "mode"}, MODE_)
-        );
+          option(vm, {"d", "death"}, &DEATH_RATE_, "Death rate"),
+          option(vm, {"l", "genome"}, &GENOME_SIZE_, "Genome size"),
+          option(vm, {"m", "mode"}, &MODE_)
+        ).doc("Individual");
     }
     static double DEATH_RATE_;
     static unsigned GENOME_SIZE_;
@@ -96,15 +121,11 @@ int main(int argc, char* argv[]) {
     const auto program = argv[0];
     std::vector<std::string> arguments(argv + 1, argv + argc);
 
-    enum class mode_t {run, help, version};
-    mode_t mode = mode_t::run;
     nlohmann::json vm;
-
     clipp::group general_options = clipp::with_prefixes_short_long("-", "--",
-      clipp::option("h", "help").set(mode, mode_t::help).doc("Print help"),
-      clipp::option("version").set(mode, mode_t::version).doc("Print version"),
-      clipp_group(vm, {"p", "pi"}, 3.1415, "Mathematical constant"),
-      clipp::option("v", "verbose")
+      option_bool(vm, {"h", "help"}, "Print help"),
+      option_bool(vm, {"version"}, "Print version"),
+      option(vm, {"pi"}, 3.1415, "Mathematical constant")
     );
     auto cli = (
       general_options,
@@ -114,22 +135,24 @@ int main(int argc, char* argv[]) {
 
     auto fmt = clipp::doc_formatting{}
         .first_column(2)
-        .doc_column(24);
+        .doc_column(24)
+        .last_column(80)
+        .indent_size(4)
+        .flag_separator(",")
+    ;
 
     if (!parsed) {
         std::cout << clipp::make_man_page(cli, program, fmt);
         std::cout << "Error: unknown argument\n";
         return 1;
     }
-    switch (mode) {
-      case mode_t::help:
+    if (vm["help"]) {
         std::cout << clipp::make_man_page(cli, program, fmt);
         return 0;
-      case mode_t::version:
+    }
+    if (vm["version"]) {
         std::cout << "clipp 1.2.0\n";
         return 0;
-      default:
-        break;
     }
     for (const auto& m: parsed) {
         std::cout << m.index() << ": " << m.arg() << " -> " << m.param();
@@ -139,4 +162,7 @@ int main(int argc, char* argv[]) {
         std::cout << '\n';
     }
     std::cout << vm.dump(2) << "\n";
+    std::cout << Individual::DEATH_RATE_ << "\n";
+    std::cout << Individual::GENOME_SIZE_ << "\n";
+    std::cout << Individual::MODE_ << "\n";
 }
