36c964e9c8170ecb2f06cd3cbadf2adbd93ce8c5
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-09-07 18:27:45 +0900

    :construction: Add functions for tests and formatting

--- cxx/clipp.cpp
+++ cxx/clipp.cpp
@@ -6,6 +6,10 @@
 #include <string>
 #include <iostream>
 
+#include <limits>
+
+namespace detail {
+
 struct nonempty {
   bool operator()(const std::string& s) {return !s.empty();}
 };
@@ -46,8 +50,8 @@ auto set(nlohmann::json& obj, const char* key) {
 }
 
 template <> inline
-auto set<double>(nlohmann::json& obj, const char* key) {
-    return [&j = obj[key]](const char* s){j = std::stod(s);};
+auto set<bool>(nlohmann::json& obj, const char* key) {
+    return [&j = obj[key]](){j = true;};
 }
 
 template <> inline
@@ -56,21 +60,43 @@ auto set<int>(nlohmann::json& obj, const char* key) {
 }
 
 template <> inline
+auto set<long>(nlohmann::json& obj, const char* key) {
+    return [&j = obj[key]](const char* s){j = std::stol(s);};
+}
+
+template <> inline
+auto set<unsigned>(nlohmann::json& obj, const char* key) {
+    return [&j = obj[key]](const char* s){j = static_cast<unsigned>(std::stoul(s));};
+}
+
+template <> inline
 auto set<unsigned long>(nlohmann::json& obj, const char* key) {
     return [&j = obj[key]](const char* s){j = std::stoul(s);};
 }
 
 template <> inline
-auto set<bool>(nlohmann::json& obj, const char* key) {
-    return [&j = obj[key]](){j = true;};
+auto set<unsigned long long>(nlohmann::json& obj, const char* key) {
+    return [&j = obj[key]](const char* s){j = std::stoull(s);};
 }
+
+template <> inline
+auto set<double>(nlohmann::json& obj, const char* key) {
+    return [&j = obj[key]](const char* s){j = std::stod(s);};
+}
+
+} // namespace detail
 
 template <class T> inline clipp::group
 option(std::vector<std::string>&& flags, T* target, const std::string& doc="", const std::string& label="arg") {
     return (
       clipp::option(std::move(flags)) &
-      clipp::value(filter_type(*target), label, *target)
-    ) % doc_default(*target, doc);
+      clipp::value(detail::filter_type(*target), label, *target)
+    ) % detail::doc_default(*target, doc);
+}
+
+inline clipp::parameter
+option_bool(std::vector<std::string>&& flags, bool* target, const std::string& doc="") {
+    return clipp::option(std::move(flags)).set(*target).doc(doc);
 }
 
 template <class T> inline clipp::group
@@ -79,14 +105,15 @@ option(nlohmann::json& obj, std::vector<std::string>&& flags, const T init, cons
     obj[key] = init;
     return (
       clipp::option(std::move(flags)) &
-      clipp::value(filter_type(init), label).call(set<T>(obj, key))
-    ) % doc_default(obj[key], doc);
+      clipp::value(detail::filter_type(init), label).call(detail::set<T>(obj, key))
+    ) % detail::doc_default(obj[key], doc);
 }
 
 template <class T> inline clipp::group
 option(nlohmann::json& obj, std::vector<std::string>&& flags, T* target, const std::string& doc="", const std::string& label="arg") {
     auto group = option(obj, std::move(flags), *target, doc, label);
-    group.back().as_param().call(clipp::set(*target));
+    auto& value = group.back().as_param();
+    value.call(clipp::set(*target));
     return group;
 }
 
@@ -94,75 +121,86 @@ inline clipp::parameter
 option_bool(nlohmann::json& obj, std::vector<std::string>&& flags, const std::string& doc="") {
     const char* key = flags.back().c_str();
     obj[key] = false;
-    return (clipp::option(flags).call(set<bool>(obj, key))).doc(doc);
+    return clipp::option(std::move(flags)).call(detail::set<bool>(obj, key)).doc(doc);
+}
+
+inline clipp::doc_formatting doc_format() {
+    return clipp::doc_formatting{}
+      .first_column(0)
+      .doc_column(24)
+      .last_column(80)
+      .indent_size(2)
+      .flag_separator(",")
+    ;
+}
+
+inline std::ostream&
+usage(std::ostream& ost,
+      const clipp::group& cli,
+      const std::string& program="PROGRAM",
+      clipp::doc_formatting fmt=doc_format()) {
+    return ost << clipp::usage_lines(cli, program, fmt) << "\n\n"
+               << clipp::documentation(cli, fmt) << "\n";
 }
 
 /////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////
 
-class Individual {
-  public:
-    static clipp::group parameter_group(nlohmann::json& vm) {
-        return clipp::with_prefixes_short_long("-", "--",
-          option(vm, {"d", "death"}, &DEATH_RATE_, "Death rate"),
-          option(vm, {"l", "genome"}, &GENOME_SIZE_, "Genome size"),
-          option(vm, {"m", "mode"}, &MODE_)
-        ).doc("Individual");
-    }
-    static double DEATH_RATE_;
-    static unsigned GENOME_SIZE_;
-    static std::string MODE_;
+struct Parameters {
+    bool BOOL = false;
+    int INT = std::numeric_limits<int>::max();
+    long LONG = std::numeric_limits<long>::max();
+    unsigned UNSIGNED = std::numeric_limits<unsigned>::max();
+    size_t SIZE_T = std::numeric_limits<size_t>::max();
+    double DOUBLE = 0.0;
+    std::string STRING = "Hello, world!";
 };
 
-double Individual::DEATH_RATE_ = 0.1;
-unsigned Individual::GENOME_SIZE_ = 42;
-std::string Individual::MODE_ = "normal";
+inline clipp::group test_types(nlohmann::json& vm, Parameters& p) {
+    return clipp::with_prefixes_short_long("-", "--",
+        option_bool(vm, {"b", "bool"}),
+        option(vm, {"i", "int"}, &p.INT),
+        option(vm, {"l", "long"}, &p.LONG),
+        option(vm, {"u", "unsigned"}, &p.UNSIGNED),
+        option(vm, {"s", "size_t"}, &p.SIZE_T),
+        option(vm, {"d", "double"}, &p.DOUBLE),
+        option(vm, {"c", "string"}, &p.STRING)
+    ).doc("Supported types:");
+}
 
 int main(int argc, char* argv[]) {
     const auto program = argv[0];
     std::vector<std::string> arguments(argv + 1, argv + argc);
 
-    nlohmann::json vm;
+    bool help = false;
+    bool version = false;
+    int answer = 42;
     clipp::group general_options = clipp::with_prefixes_short_long("-", "--",
-      option_bool(vm, {"h", "help"}, "Print help"),
-      option_bool(vm, {"version"}, "Print version"),
-      option(vm, {"pi"}, 3.1415, "Mathematical constant")
-    );
+      option_bool({"h", "help"}, &help, "Print help"),
+      option_bool({"version"}, &version, "Print version"),
+      option({"a", "answer"}, &answer, "Answer")
+    ).doc("Not stored in json:");
+
+    nlohmann::json vm;
+    Parameters p;
     auto cli = (
       general_options,
-      Individual::parameter_group(vm)
+      test_types(vm, p)
     );
     auto parsed = clipp::parse(arguments, cli);
-
-    auto fmt = clipp::doc_formatting{}
-        .first_column(2)
-        .doc_column(24)
-        .last_column(80)
-        .indent_size(4)
-        .flag_separator(",")
-    ;
-
     if (!parsed) {
-        std::cout << clipp::make_man_page(cli, program, fmt);
-        std::cout << "Error: unknown argument\n";
+        usage(std::cout, cli, program)
+          << "Error: unknown argument\n";
         return 1;
     }
-    if (vm["help"]) {
-        std::cout << clipp::make_man_page(cli, program, fmt);
+    clipp::debug::print(std::cerr, parsed);
+    if (help) {
+        usage(std::cout, cli, program);
         return 0;
     }
-    if (vm["version"]) {
+    if (version) {
         std::cout << "clipp 1.2.0\n";
         return 0;
     }
-    for (const auto& m: parsed) {
-        std::cout << m.index() << ": " << m.arg() << " -> " << m.param();
-        std::cout << " repeat #" << m.repeat();
-        if (m.blocked()) std::cout << " blocked";
-        if (m.conflict()) std::cout << " conflict";
-        std::cout << '\n';
-    }
+    std::cerr << "Answer: " << answer << "\n";
     std::cout << vm.dump(2) << "\n";
-    std::cout << Individual::DEATH_RATE_ << "\n";
-    std::cout << Individual::GENOME_SIZE_ << "\n";
-    std::cout << Individual::MODE_ << "\n";
 }
