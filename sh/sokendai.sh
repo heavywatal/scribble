f91bf01ed1570ec36c2e86d3b4c8767fc470bf3e
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-10-04 16:34:04 +0900

    :sparkles: Create Sokendai logo with characters

---
+++ sh/sokendai.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+printf "
+         AI
+SO    ND/￣
+￣\KE/￣
+   ￣
+            A I
+S O     N D/￣
+ ￣\K E/￣
+    ￣
+            AI
+            ——
+SO      ND /
+——      ——
+  \ KE /
+    ——
+            A I
+             ——
+S O     N D /
+——    E  ——
+  \ K  /
+    ——
+"
