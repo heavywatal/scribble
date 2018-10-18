3d36e54f2415621f18d36f9cd54788c4299b3398
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-10-18 15:35:26 +0900

    :construction: Add hashmap.cpp

---
+++ cxx/hashmap.cpp
@@ -0,0 +1,29 @@
+#include <unordered_map>
+#include <iostream>
+
+int main() {
+    // load_factor() == size() / bucket_count();
+    std::unordered_map<int, double> dict;
+    std::cout << "bucket_count: " << dict.bucket_count()
+              << " / " << dict.max_bucket_count() << std::endl;
+    std::cout << "load_factor: " << dict.load_factor()
+              << " / " << dict.max_load_factor() << std::endl;
+    dict.reserve(1000);
+    std::cout << "bucket_count: " << dict.bucket_count()
+              << " / " << dict.max_bucket_count() << std::endl;
+    std::cout << "load_factor: " << dict.load_factor()
+              << " / " << dict.max_load_factor() << std::endl;
+    for (size_t i=0; i<1000u; ++i) {
+        dict.emplace(i, i * 0.1);
+    }
+    std::cout << "bucket_count: " << dict.bucket_count()
+              << " / " << dict.max_bucket_count() << std::endl;
+    std::cout << "load_factor: " << dict.load_factor()
+              << " / " << dict.max_load_factor() << std::endl;
+    dict.reserve(10000);
+    std::cout << "bucket_count: " << dict.bucket_count()
+              << " / " << dict.max_bucket_count() << std::endl;
+    std::cout << "load_factor: " << dict.load_factor()
+              << " / " << dict.max_load_factor() << std::endl;
+    return 0;
+}
