ef4dc5bf94bdc4efecb91d91105808ebfdf8c5a2
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-10-23 13:29:54 +0900

    :construction: Improve handling of positional arguments

--- pbs/qsub.py
+++ pbs/qsub.py
@@ -7,13 +7,13 @@ import subprocess
 import os
 
 
-def make_script(args, rest):
-    executable = os.path.abspath(args.executable)
-    assert os.access(executable, os.X_OK)
+def make_script(args):
+    args[0] = os.path.abspath(args[0])
+    assert os.access(args[0], os.X_OK)
     lines = [
         '#!/bin/sh',
         'cd $PBS_O_WORKDIR',
-        ' '.join([executable] + rest)
+        ' '.join(args)
     ]
     return '\n'.join(lines) + '\n'
 
@@ -24,17 +24,18 @@ def main():
     parser.add_argument('-N', '--jobname')
     parser.add_argument('-j', '--ncpus', default=1, type=int)
     parser.add_argument('-m', '--mem', default=1, type=int)
-    parser.add_argument('executable')
-    (args, rest) = parser.parse_known_args()
-    jobname = args.jobname or os.path.basename(args.executable)
+    parser.add_argument('positional', nargs='+')
+    args = parser.parse_args()
+    executable = args.positional[0]
+    jobname = args.jobname or os.path.basename(executable)
     resource = 'select=1:ncpus={0.ncpus}:mem={0.mem}gb'.format(args)
     command = f'qsub -j oe -N {jobname} -l {resource}'
-    if args.executable.endswith('.R'):
+    if executable.endswith('.R'):
         qsub_R = os.path.join(os.path.dirname(__file__), 'qsub.R')
-        command += f' -v NCPUS={args.ncpus},INFILE={args.executable} {qsub_R}'
+        command += f' -v NCPUS={args.ncpus},INFILE={executable} {qsub_R}'
         input = None
     else:
-        script = make_script(args, rest)
+        script = make_script(args.positional)
         input = script.encode()
         print(script)
     print(command)
