e2ed5bcc6f264c5145d5b1fd615363c171723dc7
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-12-23 23:53:05 +0900

    :sparkles: Add colorbrewer to palette.R

--- rstats/palette.R
+++ rstats/palette.R
@@ -4,7 +4,7 @@ col2luminance = function(col) {
 
 viridis_options = c("magma", "inferno", "plasma", "viridis", "cividis")
 
-df_raw = purrr::map_dfr(viridis_options, function(option) {
+df_viridis_raw = purrr::map_dfr(viridis_options, function(option) {
   purrr::map_dfr(seq.int(2L, 8L), function(n) {
     tibble::tibble(
       option = option, n = n,
@@ -14,19 +14,56 @@ df_raw = purrr::map_dfr(viridis_options, function(option) {
   })
 }) %>% print()
 
-df = df_raw %>% dplyr::mutate(
+df_viridis = df_viridis_raw %>% dplyr::mutate(
   color = stringr::str_replace(color, "FF$", ""),
   luminance = col2luminance(color),
   textcolor = ifelse(luminance < 0.5, "#ffffff", "#000000")
 ) %>% print()
 
-p = df %>% ggplot(aes(n, y)) +
+p = df_viridis %>% ggplot(aes(n, y)) +
     geom_tile(aes(fill = color)) +
-    geom_text(aes(label = color, colour = textcolor)) +
+    geom_text(aes(label = color, colour = textcolor), family = "Ubuntu Mono") +
     scale_fill_identity() +
     scale_colour_identity() +
     facet_wrap(~option, ncol = 1L) +
     theme_void()
 p
+ggsave("viridis.pdf", p, width = 6, height = 9, device = cairo_pdf)
 
-ggsave("viridis.pdf", p, width = 6, height = 9)
+# #######1#########2#########3#########4#########5#########6#########7#########
+
+brewer_pal = RColorBrewer::brewer.pal.info %>%
+  tibble::rownames_to_column(var = "palette") %>%
+  tibble::as_tibble() %>%
+  dplyr::select(-maxcolors) %>%
+  dplyr::filter(category != "seq") %>%
+  print()
+
+df_brewer_raw = brewer_pal %>%
+  dplyr::mutate(data = purrr::map(palette, function(palette) {
+    purrr::map_dfr(seq.int(2L, 8L), function(n) {
+      tibble::tibble(
+        n = n,
+        color = scales::brewer_pal(palette = palette)(n),
+        y = seq_along(color)
+      )
+    })
+  })) %>%
+  tidyr::unnest() %>%
+  print()
+
+df_brewer = df_brewer_raw %>% dplyr::mutate(
+  palette = factor(palette, levels = brewer_pal$palette),
+  luminance = col2luminance(color),
+  textcolor = ifelse(luminance < 0.5, "#ffffff", "#000000")
+) %>% print()
+
+p = df_brewer %>% ggplot(aes(n, y)) +
+    geom_tile(aes(fill = color)) +
+    geom_text(aes(label = color, colour = textcolor), family = "Ubuntu Mono") +
+    scale_fill_identity() +
+    scale_colour_identity() +
+    facet_wrap(~palette, ncol = 2L, dir = "v") +
+    theme_void()
+p
+ggsave("brewer.pdf", p, width = 11, height = 14, device = cairo_pdf)
