78e838f58626a75f8fac390f3477436655a5da27
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-01-21 10:03:39 +0900

    :sparkles: Create deletelocalsnapshots.py

---
+++ python/deletelocalsnapshots.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Delete Time Machine local backups to shrink initial backup
+defaults read /Library/Preferences/com.apple.TimeMachine MaxSize
+"""
+import subprocess
+
+
+def listlocalsnapshots():
+    args = ['/usr/bin/tmutil', 'listlocalsnapshots', '/']
+    proc = subprocess.run(args, stdout=subprocess.PIPE)
+    stdout = proc.stdout.decode()
+    return stdout.split('\n')
+
+
+def deletelocalsnapshots(date):
+    args = ['/usr/bin/tmutil', 'deletelocalsnapshots', date]
+    subprocess.run(args)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--dry-run', action='store_true')
+    args = parser.parse_args()
+    snapshots = listlocalsnapshots()
+    dates = [x.rsplit('.', 1)[-1] for x in snapshots if x]
+    print(dates)
+    for date in dates:
+        print(date)
+        if not args.dry_run:
+            deletelocalsnapshots(date)
+
+
+if __name__ == '__main__':
+    main()
