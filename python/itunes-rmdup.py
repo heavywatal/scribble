5bb711165ce876ad833d6f95cbfd86ead989ed2a
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-01-26 01:09:36 +0900

    :sparkles: Handle AssertionError

--- python/itunes-rmdup.py
+++ python/itunes-rmdup.py
@@ -10,47 +10,48 @@ import datetime
 def print_stat(file):
     dt = datetime.datetime.fromtimestamp(file.stat().st_ctime)
     time = dt.isoformat(timespec='seconds')
-    size = round(file.stat().st_size * 1e-6)
-    print(f'{time} {size:>4}MB  {file.name}')
+    size = round(file.stat().st_size * 1e-3)
+    print(f'{time} {size:>7}KB  {file.name}')
 
 
-def sort_by_ctime_size(files):
-    if files[0].stat().st_ctime < files[1].stat().st_ctime:
-        assert files[0].stat().st_size < files[1].stat().st_size
-        return files
-    else:
-        assert files[0].stat().st_size > files[1].stat().st_size
-        return reversed(files)
-
-
-def rmdup(directory, dry_run=False):
+def rmdup(directory, dry_run=False, force=False):
     dir = pathlib.Path(directory)
     tracks = dict()
     for file in sorted(dir.glob('*.m4a')):
         print_stat(file)
         key = file.name.split(' ', 1)[0]
         tracks.setdefault(key, []).append(file)
-    print('######')
+    print(tracks.keys())
+    duplicated = {k: v for k, v in tracks.items() if len(v) > 1}
+    print(f'{len(duplicated)} duplicates')
     trash = pathlib.Path('~/.Trash').expanduser()
-    for files in tracks.values():
-        if len(files) == 2:
-            old, new = sort_by_ctime_size(files)
-            if dry_run:
-                print('old: ' + str(old))
-                print('new: ' + str(new))
-            else:
-                shutil.move(str(old), trash)
-                shutil.move(str(new), str(old))
-                print_stat(old)
+    for files in duplicated.values():
+        old, new = sorted(files, key=lambda x: x.stat().st_c_time)
+        try:
+            assert old.stat().st_size < new.stat().st_size
+        except AssertionError as err:
+            print("The new one is smaller:")
+            print_stat(old)
+            print_stat(new)
+            if not force:
+                raise err
+        if dry_run:
+            print('- ' + old.name)
+            print('+ ' + new.name)
+        else:
+            shutil.move(str(old), trash)
+            shutil.move(str(new), str(old))
+            print_stat(old)
 
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--dry-run', action='store_true')
+    parser.add_argument('-f', '--force', action='store_true')
     parser.add_argument('directory')
     args = parser.parse_args()
-    rmdup(args.directory, args.dry_run)
+    rmdup(args.directory, args.dry_run, args.force)
 
 
 if __name__ == '__main__':
