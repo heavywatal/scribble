98448d41ef128cd4f9056db196b248eec4d8ae32
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-02-01 18:35:28 +0900

    :zap: Investigate igraph::degree and igraph::vertex_attr

--- rstats/igraph.R
+++ rstats/igraph.R
@@ -6,10 +6,16 @@ graph = igraph::graph_from_edgelist(el)
 
 graph = igraph::make_tree(N)
 
-outdegree_zero = function(graph) {
+outdegree_zero_v = function(graph) {
   vs = igraph::V(graph)
   deg = igraph::degree(graph, vs, mode = "out", loops = FALSE)
   tumopp:::as_ids(vs)[deg == 0L]
+}
+
+outdegree_zero_s = function(graph) {
+  vs = seq_len(igraph::gorder(graph))
+  deg = igraph::degree(graph, vs, mode = "out", loops = FALSE)
+  vs[deg == 0L]
 }
 
 not_in_parents = function(graph) {
@@ -18,6 +24,18 @@ not_in_parents = function(graph) {
 }
 
 bench::mark(
-  outdegree_zero(graph),
-  not_in_parents(graph)
+  outdegree_zero_v(graph),
+  outdegree_zero_s(graph),
+  not_in_parents(graph),
+  check = FALSE
 )
+
+# #######1#########2#########3#########4#########5#########6#########7#########
+
+igraph::vertex_attr
+
+gattr = function(graph) {
+  mybracket2 = utils::getFromNamespace("C_R_igraph_mybracket2", "igraph")
+  .Call(mybracket2, graph, 9L, 3L)
+}
+gattr(graph)$name
