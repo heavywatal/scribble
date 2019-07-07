6f790597d0822dd7c5dbf383014eb46cee43c022
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-07-07 11:28:04 +0900

    :sparkles: Add map and unordered_map to sizeof.cpp

--- cxx/sizeof.cpp
+++ cxx/sizeof.cpp
@@ -6,6 +6,9 @@
 #include <valarray>
 #include <array>
 #include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
 #include <tuple>
 #include <memory>
 #include <iostream>
@@ -34,16 +37,18 @@ inline void container() {
     std::cout << "sizeof(container) ----------------\n";
     std::vector<int> vi(7);
     std::cout << "vector<int>     " << sizeof(vi) << " + data\n";
-    std::vector<long> vl(7);
-    std::cout << "vector<long>    " << sizeof(vl) << " + data\n";
     std::deque<int> di(7);
     std::cout << "deque<int>      " << sizeof(di) << " + data\n";
     std::valarray<int> vai(0, 7);
     std::cout << "valarray<int>   " << sizeof(vai) << " + data\n";
-    std::array<int, 7> ai3;
-    std::cout << "array<int, 7>   " << sizeof(ai3) << "\n";
     std::set<int> si;
     std::cout << "set<int>        " << sizeof(si) << " + data\n";
+    std::map<int,int> mii;
+    std::cout << "map<int,int>    " << sizeof(mii) << " + data\n";
+    std::unordered_set<int> uosi;
+    std::cout << "uo_set<int>     " << sizeof(uosi) << " + data\n";
+    std::unordered_map<int,int> uomii;
+    std::cout << "uo_map<int,int> " << sizeof(uomii) << " + data\n";
     std::pair<int, int> pii;
     std::cout << "pair<int,int>   " << sizeof(pii) << "\n";
     std::pair<int, long> pil;
@@ -54,6 +59,10 @@ inline void container() {
     std::cout << "tuple<int,int>  " << sizeof(tii) << "\n";
     std::tuple<int, long> til;
     std::cout << "tuple<int,long> " << sizeof(til) << "\n";
+    std::array<int, 7> ai7;
+    std::cout << "array<int,7>    " << sizeof(ai7) << "\n";
+    std::array<long, 7> al7;
+    std::cout << "array<long,7>   " << sizeof(al7) << "\n";
     std::bitset<1> bs1;
     std::cout << "bitset<1>       " << sizeof(bs1) << "\n";
     std::bitset<65> bs65;
