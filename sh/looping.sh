bf886f644c1fbbe206c2fcaa5919b81484d1828d
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2020-02-03 21:39:30 +0900

    :sparkles: Create looping.sh

---
+++ sh/looping.sh
@@ -0,0 +1,19 @@
+for x in 1 2 3; do
+  echo $x
+done
+# 1
+# 2
+# 3
+
+for x in "1 2 3"; do
+  echo $x
+done
+# 1 2 3
+
+VARIABLE="1 2 3"
+for x in $VARIABLE; do
+  echo $x
+done
+# zsh in "1 2 3"
+# bash in 1 2 3
+# dash in 1 2 3
