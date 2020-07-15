de2caf5cbe7fa5ade6c2dd64afafbe4e0e6afc82
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2020-07-16 02:44:31 +0900

    :zap: Benchmark print.tbl() and reduce()

--- rstats/tibble.R
+++ rstats/tibble.R
@@ -4,3 +4,31 @@ library(tidyverse)
 vec = c(answer = 42L, whoami = 24601L)
 dplyr::bind_rows(vec)
 tibble::as_tibble(t(vec))
+
+# #######1#########2#########3#########4#########5#########6#########7#########
+
+bench::mark(
+  tibble = wtl::redirect(tibble:::print.tbl(ggplot2::diamonds)),
+  data.table = wtl::redirect(data.table:::print.data.table(ggplot2::diamonds)),
+  wtl = wtl::redirect(wtl:::printdf(ggplot2::diamonds))
+)
+
+# #######1#########2#########3#########4#########5#########6#########7#########
+
+df = diamonds %>% select(where(is.numeric))
+
+forloop = function(.x, .f) {
+  y = numeric(nrow(.x))
+  for (col in .x) {
+    y = .f(y, col)
+  }
+  y
+}
+
+bench::mark(
+  Reduce = Reduce(`+`, df),
+  reduce = reduce(df, `+`),
+  rowSums = rowSums(df),
+  apply = apply(df, 1, sum),
+  "for" = forloop(df, `+`)
+)
