3f2a0e9d77b8668e2e8f8a0208f6d698c16b24b7
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2021-03-11 16:23:33 +0900

    :sparkles: Try rust with reqwest and scraper

---
+++ rust/weathernews/src/channel.rs
@@ -0,0 +1,19 @@
+fn get_text() -> Result<String, reqwest::Error> {
+    let channel_url = "https://ch.nicovideo.jp/weathernews";
+    println!("get {}", channel_url);
+    Ok(reqwest::blocking::get(channel_url)?.text()?)
+}
+
+fn select_latest(body: &str) -> Vec<String> {
+    let document = scraper::Html::parse_document(&body);
+    let selector = scraper::Selector::parse(".thumb_live.thumb_100").unwrap();
+    let elements = document.select(&selector);
+    elements.map(|e| e.value().attr("href").unwrap().to_string()).collect()
+}
+
+pub fn get_latest_url() -> String {
+    let body = get_text().unwrap();
+    let urls = select_latest(&body);
+    println!("{:?}", urls);
+    urls[0].to_string()
+}
---
+++ rust/weathernews/src/main.rs
@@ -0,0 +1,17 @@
+mod channel;
+use std::process::Command;
+
+fn open(url: &str) {
+    println!("open -a Safari {}", url);
+    Command::new("open")
+            .arg("-a")
+            .arg("Safari")
+            .arg(url)
+            .spawn()
+            .expect("failed to execute process");
+}
+
+fn main() {
+    let url = channel::get_latest_url();
+    open(&url);
+}
