eb93716531281afd7c12f153cf1632a8830ce920
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2021-05-10 21:07:08 +0900

    :sparkles: Create simple envvars.sh with array

---
+++ qsub/uge/envvars.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -S /bin/sh
+#$ -l short
+#$ -l s_vmem=2G,mem_req=2G
+#$ -pe def_slot 2
+#$ -t 1-2
+#$ -cwd
+
+echo "pwd: $(pwd)"
+echo HOME: $HOME
+echo USER: $USER
+echo JOB_ID: $JOB_ID
+echo JOB_NAME: $JOB_NAME
+echo HOSTNAME: $HOSTNAME
+echo SGE_TASK_ID: $SGE_TASK_ID
+echo SGE_TASK_FIRST: $SGE_TASK_FIRST
+echo SGE_TASK_LAST: $SGE_TASK_LAST
+echo SGE_TASK_STEPSIZE: $SGE_TASK_STEPSIZE
