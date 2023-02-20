01ca1559bf89dcd31acf3d6a5413a84c0b08d5dd
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-02-20 13:54:10 +0900

    :sparkles: Create dynamic wallpaper with Equinox.app

---
+++ rstats/visualize/wallpaper.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+
+scale_gray = function(x, start = 0.20, end = 0.80) {
+  (x - min(x)) * (end - start) / (max(x) - min(x)) + start
+}
+
+radius = 53
+
+df = tibble::tibble(
+  azimuth = seq.int(0L, 360L, 1L),
+  theta = azimuth * pi / 180,
+  y = -cos(theta),
+  altitude = radius * y
+) |>
+  dplyr::mutate(level = scale_gray(y)) |>
+  dplyr::mutate(color = gray(level)) |>
+  print()
+
+outdir = fs::path("wallpaper")
+fs::dir_create(outdir)
+
+df_use = df |>
+  dplyr::filter(azimuth %in% seq.int(0L, 180L, length.out = 13L)) |>
+  dplyr::mutate(across(where(is.double), round, 3)) |>
+  readr::write_tsv(outdir / "equinox.tsv") |>
+  print()
+
+p = ggplot(df) +
+  aes(azimuth, altitude, color = color) +
+  geom_point(shape = 16, stroke = 0) +
+  geom_point(data = df_use, size = 4, shape = 16, stroke = 0) +
+  scale_x_continuous(breaks = seq.int(0, 360, 90)) +
+  scale_color_identity()
+p
+ggsave(outdir / "equinox.png", p, width = 6, height = 4) |>
+  wtl::oxipng()
+
+p_void = ggplot() + theme_void()
+df_use |>
+  dplyr::mutate(outfile = outdir / sprintf("gray-%04d.png", round(1000 * level))) |>
+  purrr::pwalk(\(outfile, color, ...) {
+    ggsave(outfile, p_void, bg = color, width = 16, height = 9, dpi = 100) |>
+      wtl::oxipng() |>
+      print()
+  })
