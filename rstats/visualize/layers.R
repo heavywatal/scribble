04768ce1b0abcd99fba9dc891ef8c98b4d9f79df
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-04-05 15:08:01 +0900

    :lipstick: Update code and colors for ggplot-layers.png

--- rstats/visualize/layers.R
+++ rstats/visualize/layers.R
@@ -1,6 +1,6 @@
 delta = 0.3
 
-df_label = tribble(
+df_label = tibble::tribble(
   ~key, ~label,
   "Data", "iris, diamonds",
   "Aesthetics", "x, y, color",
@@ -9,35 +9,41 @@ df_label = tribble(
   "Statistics", "smooth, bin",
   "Coordinates", "cartesian, polar",
   "Theme", "gray, classic"
-) %>%
-  tibble::rowid_to_column("z") %>%
-  dplyr::mutate(x = 0, y = delta * z) %>%
-  dplyr::mutate(z = as.character(z)) %>%
+) |>
+  tibble::rowid_to_column("z") |>
+  dplyr::mutate(x = 0, y = delta * z) |>
   print()
 
 rad = 0.8
 theta = pi / 6
-df_unit = tibble(
+df_unit = tibble::tibble(
   x = c(0, 1, 1 + rad * cos(theta), rad * cos(theta)),
-  y = c(0, 0, -rad * sin(theta), -rad * sin(theta)))
-
-df = df_label$z %>%
-  purrr::map_dfr(~df_unit, .id = "z") %>%
-  dplyr::mutate(z = as.integer(z)) %>%
-  dplyr::mutate(y = y + delta * z) %>%
-  dplyr::mutate(z = as.character(z)) %>%
+  y = c(0, 0, -rad * sin(theta), -rad * sin(theta))) |>
   print()
 
-dev.off()
-quartz("layers", 5, 5)
+df = df_label |>
+  dplyr::select(z) |>
+  dplyr::mutate(data = purrr::map(z, \(x) df_unit)) |>
+  tidyr::unnest_longer(data) |>
+  tidyr::unpack(data) |>  # why needed?
+  dplyr::mutate(y = y + delta * z) |>
+  print()
+
 p = ggplot(df) +
   aes(x, y) +
   geom_polygon(aes(group = z, fill = z), alpha = 0.66) +
-  geom_text(aes(label = key, color = z), df_label, nudge_x = -0.05, nudge_y = -0.06, hjust = 1, size = 6, family = "Helvetica Neue Bold") +
-  geom_text(aes(label = label), df_label, nudge_x = -0.05, nudge_y = -0.16, hjust = 1, size = 4, color = "#555555", family = "Helvetica Neue Medium") +
+  geom_text(aes(label = key, color = z), df_label, hjust = 1, size = 6,
+    nudge_x = -0.05, nudge_y = -0.06, family = "Helvetica Neue Bold") +
+  geom_text(aes(label = label), df_label, hjust = 1, size = 4, color = "#555555",
+    nudge_x = -0.05, nudge_y = -0.16, family = "Helvetica Neue Medium") +
   scale_x_continuous(expand = expansion(0.01)) +
   scale_y_continuous(expand = expansion(0.01)) +
+  scale_color_viridis_c(option = "turbo", begin = 0.02, end = 0.99) +
+  scale_fill_viridis_c(option = "turbo", begin = 0.02, end = 0.99) +
   coord_fixed(xlim = c(-0.75, 1 + rad * cos(theta))) +
   theme_void() + theme(legend.position = "none")
 p
-ggsave("ggplot-layers.png", p, width = 5, height = 5)
+
+# ragg cannot use "Helvetica Neue Medium"
+ggsave("ggplot-layers.png", p, width = 5, height = 5, dev = grDevices::png) |>
+  wtl::oxipng()
