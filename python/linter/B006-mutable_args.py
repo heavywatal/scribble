f07b93660cc004bb3132938be5ce60d27906d317
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-06-08 00:39:25 +0900

    :memo: Mutable argument default is dangerous

---
+++ python/linter/B006-mutable_args.py
@@ -0,0 +1,27 @@
+# ruff: noqa: S101
+from collections.abc import Sequence
+
+
+def dangerous(mutarg: list[int] = []):  # noqa: B006
+    mutarg.append(1)
+    return mutarg
+
+
+assert dangerous() == [1]  # initialize the default [] only once
+assert dangerous() == [1, 1]  # surprising!
+assert dangerous([2]) == [2, 1]
+assert dangerous() == [1, 1, 1]  # surprising!
+
+
+def partial_safety_by_annotation(pseudo_immut: Sequence[str] = ()):
+    """Can receive mutable as well, but linter blames mutation."""
+    # pseudo_immut.append("blamed")
+    if isinstance(pseudo_immut, list):
+        pseudo_immut.append("not blamed")
+    print(f"{pseudo_immut=}")
+    return pseudo_immut
+
+
+partial_safety_by_annotation()
+partial_safety_by_annotation(["list"])
+partial_safety_by_annotation(("tuple",))
