f9232a2c3b8e387db46422dd64e57c3ae9778548
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-02-12 22:34:26 +0900

    :zap: Add cpp11 to benchmark of nth

--- rstats/benchmark/nth.R
+++ rstats/benchmark/nth.R
@@ -1,25 +1,24 @@
 Rcpp::cppFunction("
-double nth_element_constref(const Rcpp::NumericVector& x, int n) {
+double nth_element_rcpp(const Rcpp::NumericVector& x, int n) {
     Rcpp::NumericVector copy(Rcpp::clone(x));
-    auto it = copy.begin() + (n - 1);
+    auto it = copy.begin() + n;
     std::nth_element(copy.begin(), it, copy.end());
     return *it;
 }
 ")
 
 Rcpp::cppFunction("
-double nth_element_ref(Rcpp::NumericVector& x, int n) {
-    Rcpp::NumericVector copy(Rcpp::clone(x));
-    auto it = copy.begin() + (n - 1);
+double nth_element_rcpp_std(std::vector<double> copy, int n) {
+    auto it = copy.begin() + n;
     std::nth_element(copy.begin(), it, copy.end());
     return *it;
 }
 ")
 
-Rcpp::cppFunction("
-double nth_element(Rcpp::NumericVector x, int n) {
-    auto it = x.begin() + (n - 1);
-    std::nth_element(x.begin(), it, x.end());
+cpp11::cpp_function("
+double nth_element11(std::vector<double> copy, int n) {
+    auto it = copy.begin() + n;
+    std::nth_element(copy.begin(), it, copy.end());
     return *it;
 }
 ")
@@ -28,14 +27,15 @@ nth_base = function(x, n) {
   sort(x, partial = n)[n]
 }
 
-size = 10000L
-i = 10L
+size = 100000L
+i = 10000L
 x = runif(size)
 
 bench::mark(
-  constref = x < nth_element_constref(x, i),
-  ref = x < nth_element_ref(x, i),
-  cpp = x < nth_element(x, i),
-  base = x < nth_base(x, i),
-  vctrs = i > vctrs::vec_rank(x)
+  rcpp = x < nth_element_rcpp(x, i),
+  rcppstd = x < nth_element_rcpp_std(x, i),
+  cpp11 = x < nth_element11(x, i),
+  tumopp = x < tumopp:::nth_element(x, i),
+  base = x < nth_base(x, i + 1L),
+  vctrs = (i + 1L) > vctrs::vec_rank(x)
 )
