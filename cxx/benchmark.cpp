16cdeb97e70187195a746ec44fce64efa9fd4bb4
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2024-11-23 23:18:11 +0900

    :wrench: Add pcglite to benchmark

--- cxx/benchmark.cpp
+++ cxx/benchmark.cpp
@@ -3,6 +3,7 @@
 #include <wtl/chrono.hpp>
 #include <wtl/resource.hpp>
 #include <wtl/iostr.hpp>
+#include <pcglite/pcglite.hpp>
 #include <pcg/pcg_random.hpp>
 
 #include <sfmt.hpp>
@@ -10,71 +11,76 @@
 #include <functional>
 
 inline void random_uint32() {
+    pcglite::pcg32 rng32{};
+    pcglite::pcg64 rng64{};
     constexpr size_t n = 4 * 1000 * 1000;
     uint32_t trash = 0;
     std::cout << wtl::diff_rusage([&](){
         for (size_t i=0; i<n; ++i) {
-            trash += static_cast<uint32_t>(wtl::sfmt64()());
+            trash += static_cast<uint32_t>(rng64());
         }
     }, 3u) << "\t""static_cast32(gen64)""\t" << trash << std::endl;
     std::cout << wtl::diff_rusage([&](){
         for (size_t i=0; i<n; ++i) {
-            trash += wtl::sfmt()();
+            trash += rng32();
         }
     }, 3u) << "\t""gen32::operator()""\t" << trash << std::endl;
     std::cout << wtl::diff_rusage([&](){
         std::uniform_int_distribution<uint32_t> unif;
         for (size_t i=0; i<n; ++i) {
-            trash += unif(wtl::sfmt64());
+            trash += unif(rng64);
         }
     }, 3u) << "\t""unif32(gen64)""\t" << trash << std::endl;
 }
 
 inline void random_canonical() {
+    pcglite::pcg32 rng32{};
+    pcglite::pcg64 rng64{};
     constexpr size_t n = 16 * 1000 * 1000;
     double trash = 0;
     std::cout << wtl::diff_rusage([&](){
         for (size_t i=0; i<n; ++i) {
-            trash += wtl::generate_canonical(wtl::sfmt64());
+            trash += wtl::generate_canonical(rng64);
         }
     }, 3u) << "\t""wtl::generate_cannonical(gen64)""\t" << trash << std::endl;
     std::cout << wtl::diff_rusage([&](){
         for (size_t i=0; i<n; ++i) {
-            trash += wtl::generate_canonical(wtl::sfmt());
+            trash += wtl::generate_canonical(rng32);
         }
     }, 3u) << "\t""wtl::generate_cannonical(gen32)""\t" << trash << std::endl;
     std::cout << wtl::diff_rusage([&](){
         for (size_t i=0; i<n; ++i) {
-            trash += std::generate_canonical<double, std::numeric_limits<double>::digits>(wtl::sfmt64());
+            trash += std::generate_canonical<double, std::numeric_limits<double>::digits>(rng64);
         }
     }, 3u) << "\t""std::generate_cannonical<>(gen64)""\t" << trash << std::endl;
     std::cout << wtl::diff_rusage([&](){
         for (size_t i=0; i<n; ++i) {
-            trash += std::generate_canonical<double, std::numeric_limits<double>::digits>(wtl::sfmt());
+            trash += std::generate_canonical<double, std::numeric_limits<double>::digits>(rng32);
         }
     }, 3u) << "\t""std::generate_cannonical<>(gen32)""\t" << trash << std::endl;
 }
 
 inline void random_sample() {
+    pcglite::pcg64 rng64{};
     constexpr size_t n = 4 * 1000 * 1000;
     std::vector<size_t> x(n);
     std::iota(x.begin(), x.end(), 0u);
     size_t k = n / 40u;
     size_t trash = 0;
     std::cout << wtl::diff_rusage([&](){
-        trash += *wtl::sample(n, k, wtl::sfmt64()).begin();
+        trash += *wtl::sample(n, k, rng64).begin();
     }, 3u) << "\t""sample int""\t" << trash << std::endl;;
     std::cout << wtl::diff_rusage([&](){
-        trash += wtl::sample(x, k, wtl::sfmt64())[0];
+        trash += wtl::sample(x, k, rng64)[0];
     }, 3u) << "\t""sample""\t" << trash << std::endl;;
     std::cout << wtl::diff_rusage([&](){
-        trash += wtl::sample_floyd(x, k, wtl::sfmt64())[0];
+        trash += wtl::sample_floyd(x, k, rng64)[0];
     }, 3u) << "\t""sample_floyd""\t" << trash << std::endl;;
     std::cout << wtl::diff_rusage([&](){
-        trash += wtl::sample_fisher(x, k, wtl::sfmt64())[0];
+        trash += wtl::sample_fisher(x, k, rng64)[0];
     }, 3u) << "\t""sample_fisher""\t" << trash << std::endl;;
     std::cout << wtl::diff_rusage([&](){
-        trash += wtl::sample_knuth(x, k, wtl::sfmt64())[0];
+        trash += wtl::sample_knuth(x, k, rng64)[0];
     }, 3u) << "\t""sample_knuth""\t" << trash << std::endl;;
     x.resize(6);
     std::cout << x << std::endl;
@@ -105,6 +111,12 @@ inline void random_engine() {
       lambda_random(wtl::sfmt19937_64{});
     }, 3) << "\t""sfmt64" << std::endl;
     std::cout << wtl::diff_rusage<std::micro>([&]() {
+      lambda_random(pcglite::pcg32{});
+    }, 3) << "\t""pcglite::pcg32" << std::endl;
+    std::cout << wtl::diff_rusage<std::micro>([&]() {
+      lambda_random(pcglite::pcg64{});
+    }, 3) << "\t""pcglite::pcg64" << std::endl;
+    std::cout << wtl::diff_rusage<std::micro>([&]() {
       lambda_random(pcg32{});
     }, 3) << "\t""pcg32" << std::endl;
     std::cout << wtl::diff_rusage<std::micro>([&]() {
@@ -114,6 +126,7 @@ inline void random_engine() {
 }
 
 inline void random_binomial() {
+    pcglite::pcg64 rng64{};
     constexpr size_t n = 256u * 1024u;
     int trash = 0;
     std::binomial_distribution binom01(10000, 0.05);
@@ -122,19 +135,19 @@ inline void random_binomial() {
     std::binomial_distribution binom07(10000, 0.7);
     std::binomial_distribution binom09(10000, 0.95);
     std::cout << wtl::diff_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom01(wtl::sfmt64()); }
+        for (size_t i=0; i<n; ++i) { trash += binom01(rng64); }
     }, 3u) << "\t""binom01" << std::endl;;
     std::cout << wtl::diff_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom03(wtl::sfmt64()); }
+        for (size_t i=0; i<n; ++i) { trash += binom03(rng64); }
     }, 3u) << "\t""binom03" << std::endl;;
     std::cout << wtl::diff_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom05(wtl::sfmt64()); }
+        for (size_t i=0; i<n; ++i) { trash += binom05(rng64); }
     }, 3u) << "\t""binom05" << std::endl;;
     std::cout << wtl::diff_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom07(wtl::sfmt64()); }
+        for (size_t i=0; i<n; ++i) { trash += binom07(rng64); }
     }, 3u) << "\t""binom07" << std::endl;;
     std::cout << wtl::diff_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom09(wtl::sfmt64()); }
+        for (size_t i=0; i<n; ++i) { trash += binom09(rng64); }
     }, 3u) << "\t""binom09" << std::endl;;
     std::cerr << trash << std::endl;
 }
