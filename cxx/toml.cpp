505df0ae128025855d4aa38e75767a4919d1aca7
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-07-24 14:41:30 +0900

    :wrench: Replace toml11 with toml++

--- cxx/toml.cpp
+++ cxx/toml.cpp
@@ -1,9 +1,13 @@
-#include <toml.hpp>
+#include <filesystem>
 #include <iostream>
+#include <cstdlib>
+
+#include <toml++/toml.hpp>
 
 int main() {
-    std::string file = "/Users/watal/git/offline/config/_default/hugo.toml";
-    toml::value obj = toml::parse(file);
+    const std::filesystem::path home(std::getenv("HOME"));
+    const auto file = home / ".config" / "ruff" / "ruff.toml";
+    auto obj = toml::parse_file(file.string());
     std::cout << obj << std::endl;
     return 0;
 }
