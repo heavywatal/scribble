5d6ee7789ab9c24183c0310c864330dbe303c9fa
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-11-07 14:06:32 +0900

    build: Update deps and CMakeLists.txt

--- cxx/igraph.cpp
+++ cxx/igraph.cpp
@@ -3,16 +3,15 @@
 
 int main() {
   igraph_t graph;
-  igraph_vector_int_t v{};
   igraph_integer_t edges[] = {
     0, 1,
     0, 2
   };
-  igraph_vector_int_view(&v, edges, sizeof(edges) / sizeof(int));
+  const auto v = igraph_vector_int_view(edges, sizeof(edges) / sizeof(int));
   igraph_create(&graph, &v, 0, true);
 
   igraph_real_t res;
-  igraph_average_path_length(&graph, &res, nullptr, true, true);
+  igraph_average_path_length(&graph, nullptr, &res, nullptr, true, true);
   std::cout << res << std::endl;
   return 0;
 }
