240a6f169f3c48628a4a97121f9ca30e9e0179fa
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-11-30 23:41:32 +0900

    refactor: Replace std::cout with fmt::print in hashmap.cpp

--- cxx/hashmap.cpp
+++ cxx/hashmap.cpp
@@ -1,29 +1,22 @@
 #include <unordered_map>
-#include <iostream>
+
+#include <fmt/base.h>
 
 int main() {
     // load_factor() == size() / bucket_count();
     std::unordered_map<int, double> dict;
-    std::cout << "bucket_count: " << dict.bucket_count()
-              << " / " << dict.max_bucket_count() << std::endl;
-    std::cout << "load_factor: " << dict.load_factor()
-              << " / " << dict.max_load_factor() << std::endl;
+    fmt::println("bucket_count: {} / {}", dict.bucket_count(), dict.max_bucket_count());
+    fmt::println("load_factor: {} / {}", dict.load_factor(), dict.max_load_factor());
     dict.reserve(1000);
-    std::cout << "bucket_count: " << dict.bucket_count()
-              << " / " << dict.max_bucket_count() << std::endl;
-    std::cout << "load_factor: " << dict.load_factor()
-              << " / " << dict.max_load_factor() << std::endl;
-    for (size_t i=0; i<1000u; ++i) {
+    fmt::println("bucket_count: {} / {}", dict.bucket_count(), dict.max_bucket_count());
+    fmt::println("load_factor: {} / {}", dict.load_factor(), dict.max_load_factor());
+    for (int i=0; i<1000; ++i) {
         dict.emplace(i, static_cast<double>(i) * 0.1);
     }
-    std::cout << "bucket_count: " << dict.bucket_count()
-              << " / " << dict.max_bucket_count() << std::endl;
-    std::cout << "load_factor: " << dict.load_factor()
-              << " / " << dict.max_load_factor() << std::endl;
+    fmt::println("bucket_count: {} / {}", dict.bucket_count(), dict.max_bucket_count());
+    fmt::println("load_factor: {} / {}", dict.load_factor(), dict.max_load_factor());
     dict.reserve(10000);
-    std::cout << "bucket_count: " << dict.bucket_count()
-              << " / " << dict.max_bucket_count() << std::endl;
-    std::cout << "load_factor: " << dict.load_factor()
-              << " / " << dict.max_load_factor() << std::endl;
+    fmt::println("bucket_count: {} / {}", dict.bucket_count(), dict.max_bucket_count());
+    fmt::println("load_factor: {} / {}", dict.load_factor(), dict.max_load_factor());
     return 0;
 }
