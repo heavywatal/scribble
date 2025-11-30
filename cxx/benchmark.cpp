16031fe7f9006e4066b76ec2a1de9d1b23f2b6cd
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-12-01 00:19:03 +0900

    refactor: Use signed integers

--- cxx/benchmark.cpp
+++ cxx/benchmark.cpp
@@ -3,6 +3,7 @@
 #include <wtl/chrono.hpp>
 #include <wtl/resource.hpp>
 #include <wtl/iostr.hpp>
+#include <wtl/signed.hpp>
 #include <pcglite/pcglite.hpp>
 #include <pcg/pcg_random.hpp>
 
@@ -11,89 +12,89 @@
 inline void random_uint32() {
     pcglite::pcg32 rng32{};
     pcglite::pcg64 rng64{};
-    constexpr size_t n = 4 * 1000 * 1000;
+    constexpr int n = 4 * 1000 * 1000;
     uint32_t trash = 0;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) {
+        for (int i = 0; i < n; ++i) {
             trash += static_cast<uint32_t>(rng64());
         }
-    }, 3u) << "\t""static_cast32(gen64)""\t" << trash << std::endl;
+    }, 3) << "\t""static_cast32(gen64)""\t" << trash << std::endl;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) {
+        for (int i = 0; i < n; ++i) {
             trash += rng32();
         }
-    }, 3u) << "\t""gen32::operator()""\t" << trash << std::endl;
+    }, 3) << "\t""gen32::operator()""\t" << trash << std::endl;
     std::cout << wtl::delta_rusage([&](){
         std::uniform_int_distribution<uint32_t> unif;
-        for (size_t i=0; i<n; ++i) {
+        for (int i = 0; i < n; ++i) {
             trash += unif(rng64);
         }
-    }, 3u) << "\t""unif32(gen64)""\t" << trash << std::endl;
+    }, 3) << "\t""unif32(gen64)""\t" << trash << std::endl;
 }
 
 inline void random_canonical() {
     pcglite::pcg32 rng32{};
     pcglite::pcg64 rng64{};
-    constexpr size_t n = 16 * 1000 * 1000;
+    constexpr int n = 16 * 1000 * 1000;
     double trash = 0;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) {
+        for (int i = 0; i < n; ++i) {
             trash += wtl::generate_canonical(rng64);
         }
-    }, 5u) << "\t""wtl::generate_canonical(gen64)""\t" << trash << std::endl;
+    }, 5) << "\t""wtl::generate_canonical(gen64)""\t" << trash << std::endl;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) {
+        for (int i = 0; i < n; ++i) {
             trash += wtl::generate_canonical(rng32);
         }
-    }, 5u) << "\t""wtl::generate_canonical(gen32)""\t" << trash << std::endl;
+    }, 5) << "\t""wtl::generate_canonical(gen32)""\t" << trash << std::endl;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) {
+        for (int i = 0; i < n; ++i) {
             trash += std::generate_canonical<double, std::numeric_limits<double>::digits>(rng64);
         }
-    }, 5u) << "\t""std::generate_canonical<>(gen64)""\t" << trash << std::endl;
+    }, 5) << "\t""std::generate_canonical<>(gen64)""\t" << trash << std::endl;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) {
+        for (int i = 0; i < n; ++i) {
             trash += std::generate_canonical<double, std::numeric_limits<double>::digits>(rng32);
         }
-    }, 5u) << "\t""std::generate_canonical<>(gen32)""\t" << trash << std::endl;
+    }, 5) << "\t""std::generate_canonical<>(gen32)""\t" << trash << std::endl;
 }
 
 inline void random_sample() {
     pcglite::pcg64 rng64{};
-    constexpr size_t n = 4 * 1000 * 1000;
-    std::vector<size_t> x(n);
-    std::iota(x.begin(), x.end(), 0u);
-    size_t k = n / 40u;
-    size_t trash = 0;
+    constexpr int n = 4 * 1000 * 1000;
+    std::vector<int> x(n);
+    std::iota(x.begin(), x.end(), 0);
+    constexpr int k = n / 40;
+    int trash = 0;
     std::cout << wtl::delta_rusage([&](){
         trash += *wtl::sample(n, k, rng64).begin();
-    }, 3u) << "\t""sample int""\t" << trash << std::endl;;
+    }, 3) << "\t""sample int""\t" << trash << std::endl;;
     std::cout << wtl::delta_rusage([&](){
         trash += wtl::sample(x, k, rng64)[0];
-    }, 3u) << "\t""sample""\t" << trash << std::endl;;
+    }, 3) << "\t""sample""\t" << trash << std::endl;;
     std::cout << wtl::delta_rusage([&](){
         trash += wtl::sample_floyd(x, k, rng64)[0];
-    }, 3u) << "\t""sample_floyd""\t" << trash << std::endl;;
+    }, 3) << "\t""sample_floyd""\t" << trash << std::endl;;
     std::cout << wtl::delta_rusage([&](){
         trash += wtl::sample_fisher(x, k, rng64)[0];
-    }, 3u) << "\t""sample_fisher""\t" << trash << std::endl;;
+    }, 3) << "\t""sample_fisher""\t" << trash << std::endl;;
     std::cout << wtl::delta_rusage([&](){
         trash += wtl::sample_knuth(x, k, rng64)[0];
-    }, 3u) << "\t""sample_knuth""\t" << trash << std::endl;;
+    }, 3) << "\t""sample_knuth""\t" << trash << std::endl;;
     x.resize(6);
     std::cout << x << std::endl;
 }
 
 inline void random_engine() {
-    constexpr size_t n = 1 * 1000 * 1000;
-    std::vector<size_t> v(n);
-    std::poisson_distribution<size_t> dist(2.0);
+    constexpr int n = 1 * 1000 * 1000;
+    std::vector<int> v(n);
+    std::poisson_distribution<int> dist(2.0);
     // std::vector<double> v(n);
     // std::normal_distribution<double> dist(0.0, 1.0);
     // std::uniform_real_distribution<double> dist(0.0, 1.0);
     auto lambda_random = [&](auto&& generator) mutable {
-        for (size_t j=0; j<n; ++j) {
-            v[j] = dist(generator);
+        for (int j=0; j < n; ++j) {
+            wtl::at(v, j) = dist(generator);
         }
     };
     std::cout << wtl::delta_rusage<std::micro>([&]() {
@@ -125,7 +126,7 @@ inline void random_engine() {
 
 inline void random_binomial() {
     pcglite::pcg64 rng64{};
-    constexpr size_t n = 256u * 1024u;
+    constexpr int n = 256 * 1024;
     int trash = 0;
     std::binomial_distribution binom01(10000, 0.05);
     std::binomial_distribution binom03(10000, 0.3);
@@ -133,20 +134,20 @@ inline void random_binomial() {
     std::binomial_distribution binom07(10000, 0.7);
     std::binomial_distribution binom09(10000, 0.95);
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom01(rng64); }
-    }, 3u) << "\t""binom01" << std::endl;;
+        for (int i = 0; i < n; ++i) { trash += binom01(rng64); }
+    }, 3) << "\t""binom01" << std::endl;;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom03(rng64); }
-    }, 3u) << "\t""binom03" << std::endl;;
+        for (int i = 0; i < n; ++i) { trash += binom03(rng64); }
+    }, 3) << "\t""binom03" << std::endl;;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom05(rng64); }
-    }, 3u) << "\t""binom05" << std::endl;;
+        for (int i = 0; i < n; ++i) { trash += binom05(rng64); }
+    }, 3) << "\t""binom05" << std::endl;;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom07(rng64); }
-    }, 3u) << "\t""binom07" << std::endl;;
+        for (int i = 0; i < n; ++i) { trash += binom07(rng64); }
+    }, 3) << "\t""binom07" << std::endl;;
     std::cout << wtl::delta_rusage([&](){
-        for (size_t i=0; i<n; ++i) { trash += binom09(rng64); }
-    }, 3u) << "\t""binom09" << std::endl;;
+        for (int i = 0; i < n; ++i) { trash += binom09(rng64); }
+    }, 3) << "\t""binom09" << std::endl;;
     std::cerr << trash << std::endl;
 }
 
--- cxx/concurrent.cpp
+++ cxx/concurrent.cpp
@@ -8,11 +8,11 @@ int main() {
     std::cerr << "std::thread::hardware_concurrency(): "
               << concurrency << std::endl;
 
-    std::vector<std::future<size_t>> futures;
-    wtl::Semaphore semaphore(2u);
-    constexpr size_t n = 6u;
+    std::vector<std::future<int>> futures;
+    wtl::Semaphore semaphore(2);
+    constexpr int n = 6;
 
-    for (size_t i=0; i<n; ++i) {
+    for (int i = 0; i < n; ++i) {
         // manual lock before thread creation: thread IDs are reused
         semaphore.lock();
         futures.push_back(std::async(std::launch::async, [&semaphore, i] {
@@ -31,7 +31,7 @@ int main() {
     std::cerr << std::endl;
 
     futures.clear();
-    for (size_t i=0; i<n; ++i) {
+    for (int i = 0; i < n; ++i) {
         // RAII lock after thread creation: new thread IDs are created
         futures.push_back(std::async(std::launch::async, [&semaphore, i] {
             std::lock_guard<wtl::Semaphore> _(semaphore);
